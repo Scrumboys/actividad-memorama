1d1d46524c5d935bd49d52cf100921504e0fb7d9
Josias Emiliano <>
2023-12-01 11:51:58 -0700

    pequeños cambios

--- script.js
+++ script.js
@@ -1,74 +1,44 @@
-const selectorButtons = document.querySelectorAll('.selector-buttons');
-const imagenes = [
-    'verduras.png',
-    'papas.png',
-    'pescado.png',
-    'coco.png',
-    'agua.png',
-    'refresco.png',
-];
-const fondoImagenes = [
-    'fondoVerduras.svg',
-    'fondoPapas.svg',
-    'fondoPescado.svg',
-    'fondoCoco.svg',
-    'fondoAgua.svg',
-    'fondoRefresco.svg',
+let selecciones = [];
+let numCartas = 12;
+let puntaje = 0;
+
+
+let imagenes = [
+    'fondo_0.svg',
+    'fondo_1.svg',
+    'fondo_2.svg',
+    'fondo_3.svg',
+    'fondo_4.svg',
+    'fondo_5.svg',
 ];
 
-selectorButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const selector = document.querySelector('.selector-container');
-        let grid = button.getAttribute('grid');
-        generarCuadricula(grid);
-        selector.classList.add('hidden');
-    });
-});
-
-//FALTA CAMBIAR TODO
-const generarCuadricula = (cuadricula) => {
-    //alert("Entré e hice la cuadricula");
-    let container = document.getElementById('cartas-container');
+const generarCuadricula = () => {
+    let cartasContainer = document.getElementById('cartas-container');
     let copiasImg = imagenes.slice();
-    let copiasFondoImg = fondoImagenes.slice();
     let cartas = [];
     selecciones = [];
-    numCartas = cuadricula;
-    if(numCartas == 12){
-        container.style.gridTemplateColumns = "repeat(3, 1fr)";
-    }
-    else{
-        container.style.gridTemplateColumns = "repeat(2, 1fr)";
-    }
-    puntaje = 0;
-    container.classList.remove('fadeIn');
-    container.classList.add('fadeOut');
-    for( let numCarta = 0; numCarta < cuadricula; numCarta++ ){ 
+
+    for( let numCarta = 0 ; numCarta < numCartas; numCarta++ ){ 
         cartas.push(`
             <div class="div-carta" onclick="seleccionarCarta(${numCarta})" >
                 <div id="carta_${numCarta}" class="carta">
-                    <div id="trasera_${numCarta}" style="display: flex; align-items: center; justify-content: center;" class="cara trasera">
-                        <img src="./img/${copiasFondoImg[0]}" style="z-index: -10;" alt="">
+                    <div id="trasera_${numCarta}" class="cara trasera">
                         <img src="./img/${copiasImg[0]}" alt="">
                     </div>
                     <div class="cara frontal">
-                        <img src="./img/atrasCarta.svg" alt="">
+                    <img src="./img/logo_cartaTrasera.svg" alt="">
                     </div>
                 </div>
             </div>
         `);
+
         if( numCarta%2 === 1 ) {
             copiasImg.splice(0,1);
         }
     }
-    cartas.sort(() => Math.random() - 0.5);
-    container.innerHTML = cartas.join("");
-    container.classList.remove('fadeOut');
-    container.classList.add('fadeIn');
-}
 
-function reiniciar() {
-    generarCuadricula(numCartas);
+    cartas.sort(() => Math.random() - 0.5);
+    cartasContainer.innerHTML = cartas.join("");
 }
 
 const seleccionarCarta = (numCarta) => {
@@ -84,14 +54,15 @@ const seleccionarCarta = (numCarta) => {
     }
 }
 
+//aquí le puse un input para que mostrara el nombre a ingresar
 const deseleccionar = (selecciones) => {
-    //alert("Aqui me metí al deseleccionar");
+
     setTimeout(() => {
         let cartaUno = document.getElementById(`carta_${selecciones[0]}`);
         let cartaDos = document.getElementById(`carta_${selecciones[1]}`);
         let traseraUno = document.getElementById(`trasera_${selecciones[0]}`);
         let traseraDos = document.getElementById(`trasera_${selecciones[1]}`);
-        /*Aqui se valida que las cartas volteadas sean iguales*/
+
         if( traseraUno.innerHTML != traseraDos.innerHTML ) {
             cartaUno.style.transform = "rotateY(0deg)";
             cartaDos.style.transform = "rotateY(0deg)";
@@ -101,30 +72,10 @@ const deseleccionar = (selecciones) => {
             puntaje++;
         }
         if(puntaje === (numCartas/2)){
-            const main= document.getElementById("main");
-             const alerta = document.createElement("div");
-            alerta.classList.add("alerta");
-            alerta.id= "alerta";
-            detenertiempo();
-            alerta.innerHTML = 
-            `
-            <div class="mssgAlerta">
-                <h1>¡FELICIDADES!  HAS GANADO</h1>
-                <input type="text" id="playerName" placeholder="Ingrese su nombre:">
-                <button type="button" id="botonAlerta" onclick="reintentar()">Intentar de nuevo</button>
-            </div>
-            `;
-            main.append(alerta);
+
         }
+
     }, 800);
 }
 
-function reintentar() {
-    const playerName = document.getElementById("playerName");
-    NewUser(playerName.value);
-    const alerta = document.getElementById("alerta");
-    alerta.remove();
-    const boton=document.getElementById("btn-comenzar");
-    boton.click();
-    comenzar();
-}
+generarCuadricula();
