a426ca3c3a28acc54e1da1f26b8e37114f5758be
Josias Emiliano <>
2023-12-02 11:38:10 -0700

    funcionalidad terminada

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 const mensajesFondo = document.querySelector('#mensajes-container');
 const mensajeContainer = document.querySelector('#mensaje-container');
+const btnReinicio = document.querySelector('#btnReinicio');
+const btnCerrar = document.querySelector('#btn_cerrar');
 
 let selecciones = [];
-let numCartas = 12;
-let puntaje = 0;
 
+let numCartas = 12, puntaje = 0;
+
+let slcUno = 0, slcDos = 0;
 
 let imagenes = [
     'fondo_0.svg',
@@ -46,7 +49,7 @@ const generarCuadricula = () => {
 }
 
 const seleccionarCarta = (numCarta) => {
-    //alert("Aqui estoy seleccionando las cartas");
+    
     let carta = document.getElementById(`carta_${numCarta}`);
     if( carta.style.transform != "rotateY(180deg)" ) {
         carta.style.transform = "rotateY(180deg)";
@@ -58,22 +61,23 @@ const seleccionarCarta = (numCarta) => {
     }
 }
 
-//aquí le puse un input para que mostrara el nombre a ingresar
 const deseleccionar = (selecciones) => {
+    
+    setTimeout( async () => {
 
-    setTimeout(() => {
+        slcUno = '', slcDos = '';
         let cartaUno = document.getElementById(`carta_${selecciones[0]}`);
         let cartaDos = document.getElementById(`carta_${selecciones[1]}`);
         let traseraUno = document.getElementById(`trasera_${selecciones[0]}`);
         let traseraDos = document.getElementById(`trasera_${selecciones[1]}`);
 
+        slcUno = traseraUno;
+        slcDos = traseraDos;
+
         if( traseraUno.innerHTML != traseraDos.innerHTML ) {
             cartaUno.style.transform = "rotateY(0deg)";
             cartaDos.style.transform = "rotateY(0deg)";
         } else {
-            traseraUno.style.opacity = "70%";
-            traseraDos.style.opacity = "70%";
-
             const srcImagen = traseraUno.querySelector('img').getAttribute('src');
             const parts = srcImagen.split('/');
             const fileName = parts[parts.length - 1].split('.')[0];
@@ -81,6 +85,7 @@ const deseleccionar = (selecciones) => {
             mostrarMensaje(fileName);
             puntaje++;
         }
+
         if(puntaje === (numCartas/2)){
 
         }
@@ -88,18 +93,38 @@ const deseleccionar = (selecciones) => {
     }, 800);
 }
 
-const mostrarMensaje = (imagen) => {
+const mostrarMensaje = async (imagen) => {
 
     mensajesFondo.style.display = 'flex';
 
     imagenes.forEach(img => {
-        if(`${imagen}.svg` == img) {
-            mensajeContainer.innerHTML = `<img id="mensaje_${imagen}" src="./img/mensaje_${imagen}.svg" alt="">`
-            // const mensajeFondo = mensajesFondo.querySelector(`#mensaje_${imagen}`);
-            // mensajeFondo.classList.remove('mensaje-oculto');
+        if (`${imagen}.svg` == img) {
+            mensajeContainer.innerHTML += `
+                <img id="btn_cerrar" src="img/btn_cerrar.svg" alt="" style="position: absolute; right: 8rem; padding-top: 1.5rem; width: 2rem; z-index: 999;">
+                <img id="mensaje_${imagen}" src="./img/mensaje_${imagen}.svg" alt="">
+            `;
+            mensajeContainer.classList.add('fadeInUp');
         }
     });
-    
+
 }
+
+const deshabilitarCartas = (slcUno, slcDos) => {
+    setTimeout(() => {
+        slcUno.style.opacity = "70%";
+        slcDos.style.opacity = "70%";
+    }, 100);
+}
+
+mensajeContainer.addEventListener('click', () => {
+    mensajesFondo.style.display = 'none';
+    mensajeContainer.innerHTML = '';
+
+    deshabilitarCartas(slcUno, slcDos);
+});
+
+btnReinicio.addEventListener('click', () => {
+    generarCuadricula();
+});
 
 generarCuadricula();
